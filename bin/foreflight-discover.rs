f0090e48437f9f279f632fc6ecf383909f13fc13
Billy Lindeman <billylindeman@gmail.com>
2021-04-23 19:12:50 -0700

    Fix crc16 using custom impl! Shows up in foreflight now :D

--- bin/foreflight-discover.rs
+++ bin/foreflight-discover.rs
@@ -3,7 +3,10 @@
 use futures::{Sink, SinkExt};
 use std::error::Error;
 use std::net::SocketAddr;
+use std::time;
 use std::{env, io};
+
+use std::collections::HashSet;
 use tokio::net::UdpSocket;
 use tokio::sync::broadcast;
 use tokio_stream::{wrappers::BroadcastStream, StreamExt};
@@ -15,13 +18,18 @@ use fishfinder::adsb::gdl90;
 struct Server {
     socket: UdpSocket,
     buf: Vec<u8>,
+    clients: HashSet<String>,
+    tx: broadcast::Sender<gdl90::Message>,
 }
 
 impl Server {
     async fn run(self) -> Result<(), io::Error> {
-        let Server { socket, mut buf } = self;
-
-        tokio::task::spawn(async move || {});
+        let Server {
+            socket,
+            mut buf,
+            mut clients,
+            tx,
+        } = self;
 
         loop {
             let (len, addr) = socket.recv_from(&mut buf).await?;
@@ -31,22 +39,36 @@ impl Server {
                 len,
                 std::str::from_utf8(&buf).unwrap()
             );
+
+            let to_addr = SocketAddr::new(addr.ip(), 4000);
+            let to_addr_string = to_addr.to_string();
+
+            if !clients.contains(&to_addr_string) {
+                clients.insert(to_addr_string);
+                let rx = tx.subscribe();
+
+                let mut clients_clone = clients.clone();
+
+                tokio::spawn(async move {
+                    println!("spawning foreflight pusher: {}", to_addr);
+                    handle_foreflight_client(to_addr, rx).await.unwrap();
+                    clients_clone.remove(&to_addr.to_string());
+                });
+            }
         }
     }
 }
 
 async fn handle_foreflight_client(
-    addr: String,
+    addr: SocketAddr,
     rx: broadcast::Receiver<gdl90::Message>,
 ) -> Result<(), io::Error> {
     let sock = UdpSocket::bind("0.0.0.0:0").await?;
 
-    let dest: SocketAddr = addr.parse().expect("Unable to parse socket address");
-
     let mut framed_encoder = udp::UdpFramed::new(sock, gdl90::Encoder::new());
     let mut msg_stream = BroadcastStream::new(rx)
         .filter(Result::is_ok)
-        .map(|m| Ok((m.unwrap(), dest)));
+        .map(|m| Ok((m.unwrap(), addr)));
 
     framed_encoder.send_all(&mut msg_stream).await?;
 
@@ -55,6 +77,9 @@ async fn handle_foreflight_client(
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    pretty_env_logger::init();
+    log::set_max_level(log::LevelFilter::Trace);
+
     let addr = env::args()
         .nth(1)
         .unwrap_or_else(|| "0.0.0.0:63093".to_string());
@@ -62,10 +87,43 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let socket = UdpSocket::bind(&addr).await?;
     println!("Listening on: {}", socket.local_addr()?);
 
+    let (tx, _) = broadcast::channel(256);
+
     let server = Server {
         socket,
         buf: vec![0; 1024],
+        clients: HashSet::new(),
+        tx: tx.clone(),
     };
+
+    tokio::spawn(async move {
+        loop {
+            let msg = gdl90::Message::Heartbeat(gdl90::Heartbeat::default());
+            match tx.send(msg) {
+                Ok(ok) => {
+                    println!("sent: {:?}", ok);
+                }
+                Err(err) => {}
+            }
+
+            let msg = gdl90::Message::ForeflightIdentify(gdl90::ForeflightIdentify {
+                version: 1,
+                serial_number: 420420420,
+                device_name: [0x62, 0x69, 0x6C, 0x6C, 0x79, 0x0A, 0, 0],
+                device_name_long: [0; 16],
+                capabilities: 0,
+            });
+
+            match tx.send(msg) {
+                Ok(ok) => {
+                    println!("sent: {:?}", ok);
+                }
+                Err(err) => {}
+            }
+
+            tokio::time::sleep(time::Duration::from_secs(1)).await;
+        }
+    });
 
     // This starts the server task.
     server.run().await?;
--- src/adsb/gdl90.rs
+++ src/adsb/gdl90.rs
@@ -1,6 +1,7 @@
 use binwrite::{BinWrite, WriterOption};
 use bitfield::{Bit, BitRange};
 use bytes::BytesMut;
+use log::*;
 use std::io::{Result, Write};
 
 use adsb::ICAOAddress;
@@ -23,7 +24,7 @@ enum MessageType {
     Foreflight = 0x65,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 pub struct HeartbeatStatus {
     //Byte 1
     //Bit 7: GPS Pos Valid
@@ -56,6 +57,24 @@ pub struct HeartbeatStatus {
     timestamp_msb: bool,
 }
 
+impl Default for HeartbeatStatus {
+    fn default() -> HeartbeatStatus {
+        HeartbeatStatus {
+            uat_initialized: true,
+            ratcs: false,
+            gps_batt_low: false,
+            addr_type: false,
+            ident: false,
+            maintainance_required: false,
+            gps_pos_valid: true,
+            utc_ok: false,
+            csa_not_available: false,
+            csa_not_requested: false,
+            timestamp_msb: false,
+        }
+    }
+}
+
 impl BinWrite for HeartbeatStatus {
     fn write_options<W: Write>(&self, writer: &mut W, _options: &WriterOption) -> Result<()> {
         let mut b1 = 0u8;
@@ -78,20 +97,30 @@ impl BinWrite for HeartbeatStatus {
     }
 }
 
-#[derive(BinWrite, Clone, Copy)]
+#[derive(BinWrite, Clone, Copy, Debug)]
 pub struct Heartbeat {
     status: HeartbeatStatus,
     timestamp: u16,
     msg_counts: u16,
 }
 
-#[derive(BinWrite, Clone, Copy)]
+impl Default for Heartbeat {
+    fn default() -> Heartbeat {
+        Heartbeat {
+            status: HeartbeatStatus::default(),
+            timestamp: 0,
+            msg_counts: 0,
+        }
+    }
+}
+
+#[derive(BinWrite, Clone, Copy, Debug)]
 pub struct Ownship {}
 
-#[derive(BinWrite, Clone, Copy)]
+#[derive(BinWrite, Clone, Copy, Debug)]
 pub struct OwnshipGeometricAltitude {}
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 #[repr(u8)]
 pub enum TrafficAlertStatus {
     NoAlert = 0,
@@ -102,7 +131,7 @@ fn tas_to_u8(a: &TrafficAlertStatus) -> u8 {
     *a as u8
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 #[repr(u8)]
 pub enum TrafficAddressType {
     ADSBWithIcao = 0,
@@ -116,7 +145,7 @@ fn address_type_to_u8(a: &TrafficAddressType) -> u8 {
     *a as u8
 }
 
-#[derive(BinWrite, Clone, Copy)]
+#[derive(BinWrite, Clone, Copy, Debug)]
 pub struct Traffic {
     #[binwrite(preprocessor(tas_to_u8))]
     traffic_alert_status: TrafficAlertStatus, //Traffic Alert Status
@@ -130,19 +159,19 @@ pub struct Traffic {
 
 // Foreflight Extended Specification
 // https://www.foreflight.com/connect/spec
-#[derive(BinWrite, Clone, Copy)]
+#[derive(BinWrite, Clone, Copy, Debug)]
 pub struct ForeflightIdentify {
-    version: u8,
-    serial_number: u64,
-    device_name: [u8; 8],
-    device_name_long: [u8; 16],
-    capabilities: u8,
+    pub version: u8,
+    pub serial_number: u64,
+    pub device_name: [u8; 8],
+    pub device_name_long: [u8; 16],
+    pub capabilities: u32,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 pub struct ForeflightAHRS {}
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 pub enum Message {
     Heartbeat(Heartbeat),
     OwnshipReport(Ownship),
@@ -224,18 +253,58 @@ impl codec::Encoder<Message> for Encoder {
             }
             Message::ForeflightIdentify(msg) => {
                 id = MessageType::Foreflight as u8;
+                frame.extend_from_slice(&[0]); //foreflight sub msg id
                 msg.write(&mut frame)?;
             }
         }
 
-        let mut digest = crc16::State::<crc16::CCITT_FALSE>::new();
-        digest.update(&frame);
-        let crc = digest.get().to_le_bytes();
+        let crc = fcs_crc16_compute(&frame);
 
+        dst.extend_from_slice(&[0x7E]);
         dst.extend_from_slice(&[id]);
         dst.extend_from_slice(&frame);
-        dst.extend_from_slice(&crc);
+        dst.extend_from_slice(&(crc.to_le_bytes()));
+        dst.extend_from_slice(&[0x7E]);
+
+        let b: &[u8] = &dst;
+        trace!("sent bytes: {:X?}", b);
+        trace!("len: {}", frame.len());
 
         Ok(())
     }
+}
+
+const FCS_CRC16_TABLE: [u16; 256] = [
+    0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50a5, 0x60c6, 0x70e7, 0x8108, 0x9129, 0xa14a, 0xb16b,
+    0xc18c, 0xd1ad, 0xe1ce, 0xf1ef, 0x1231, 0x0210, 0x3273, 0x2252, 0x52b5, 0x4294, 0x72f7, 0x62d6,
+    0x9339, 0x8318, 0xb37b, 0xa35a, 0xd3bd, 0xc39c, 0xf3ff, 0xe3de, 0x2462, 0x3443, 0x0420, 0x1401,
+    0x64e6, 0x74c7, 0x44a4, 0x5485, 0xa56a, 0xb54b, 0x8528, 0x9509, 0xe5ee, 0xf5cf, 0xc5ac, 0xd58d,
+    0x3653, 0x2672, 0x1611, 0x0630, 0x76d7, 0x66f6, 0x5695, 0x46b4, 0xb75b, 0xa77a, 0x9719, 0x8738,
+    0xf7df, 0xe7fe, 0xd79d, 0xc7bc, 0x48c4, 0x58e5, 0x6886, 0x78a7, 0x0840, 0x1861, 0x2802, 0x3823,
+    0xc9cc, 0xd9ed, 0xe98e, 0xf9af, 0x8948, 0x9969, 0xa90a, 0xb92b, 0x5af5, 0x4ad4, 0x7ab7, 0x6a96,
+    0x1a71, 0x0a50, 0x3a33, 0x2a12, 0xdbfd, 0xcbdc, 0xfbbf, 0xeb9e, 0x9b79, 0x8b58, 0xbb3b, 0xab1a,
+    0x6ca6, 0x7c87, 0x4ce4, 0x5cc5, 0x2c22, 0x3c03, 0x0c60, 0x1c41, 0xedae, 0xfd8f, 0xcdec, 0xddcd,
+    0xad2a, 0xbd0b, 0x8d68, 0x9d49, 0x7e97, 0x6eb6, 0x5ed5, 0x4ef4, 0x3e13, 0x2e32, 0x1e51, 0x0e70,
+    0xff9f, 0xefbe, 0xdfdd, 0xcffc, 0xbf1b, 0xaf3a, 0x9f59, 0x8f78, 0x9188, 0x81a9, 0xb1ca, 0xa1eb,
+    0xd10c, 0xc12d, 0xf14e, 0xe16f, 0x1080, 0x00a1, 0x30c2, 0x20e3, 0x5004, 0x4025, 0x7046, 0x6067,
+    0x83b9, 0x9398, 0xa3fb, 0xb3da, 0xc33d, 0xd31c, 0xe37f, 0xf35e, 0x02b1, 0x1290, 0x22f3, 0x32d2,
+    0x4235, 0x5214, 0x6277, 0x7256, 0xb5ea, 0xa5cb, 0x95a8, 0x8589, 0xf56e, 0xe54f, 0xd52c, 0xc50d,
+    0x34e2, 0x24c3, 0x14a0, 0x0481, 0x7466, 0x6447, 0x5424, 0x4405, 0xa7db, 0xb7fa, 0x8799, 0x97b8,
+    0xe75f, 0xf77e, 0xc71d, 0xd73c, 0x26d3, 0x36f2, 0x0691, 0x16b0, 0x6657, 0x7676, 0x4615, 0x5634,
+    0xd94c, 0xc96d, 0xf90e, 0xe92f, 0x99c8, 0x89e9, 0xb98a, 0xa9ab, 0x5844, 0x4865, 0x7806, 0x6827,
+    0x18c0, 0x08e1, 0x3882, 0x28a3, 0xcb7d, 0xdb5c, 0xeb3f, 0xfb1e, 0x8bf9, 0x9bd8, 0xabbb, 0xbb9a,
+    0x4a75, 0x5a54, 0x6a37, 0x7a16, 0x0af1, 0x1ad0, 0x2ab3, 0x3a92, 0xfd2e, 0xed0f, 0xdd6c, 0xcd4d,
+    0xbdaa, 0xad8b, 0x9de8, 0x8dc9, 0x7c26, 0x6c07, 0x5c64, 0x4c45, 0x3ca2, 0x2c83, 0x1ce0, 0x0cc1,
+    0xef1f, 0xff3e, 0xcf5d, 0xdf7c, 0xaf9b, 0xbfba, 0x8fd9, 0x9ff8, 0x6e17, 0x7e36, 0x4e55, 0x5e74,
+    0x2e93, 0x3eb2, 0x0ed1, 0x1ef0,
+];
+
+pub fn fcs_crc16_compute(data: &[u8]) -> u16 {
+    let mut ret = 0u16;
+
+    for i in 0..data.len() {
+        ret = (FCS_CRC16_TABLE[(ret >> 8) as usize] ^ (ret << 8)) ^ (data[i] as u16);
+    }
+
+    ret
 }
